84f4a65bc4c89c64b8f80d038a3af60957ec14d5
isaui <isadestroyed17@gmail.com>
2024-06-07 00:24:29 +0700

    yeah

--- src/app/components/modules/BuatLaporanModule/sections/HeaderSection.tsx
+++ src/app/components/modules/BuatLaporanModule/sections/HeaderSection.tsx
@@ -1,16 +1,14 @@
 import { FaArrowLeft } from "react-icons/fa"
 
 const HeaderSection = ()=> {
-    return <div className="grid grid-cols-3  w-full  text-black">
-        <div className="flex w-full h-full">
+    return <div className="grid grid-cols-3  w-full  text-black ">
+        <div className="flex w-full  h-full">
         <FaArrowLeft className="my-auto"/>
         </div>
-        <div className=" self-center my-auto text-2xl font-bold">
+        <div className=" self-center text-center my-auto text-2xl font-bold">
             <h1>Buat Laporan</h1>
         </div>
-        <div>
-
-        </div>
+       <div></div>
     </div>
 }
 export default HeaderSection
--- src/app/components/modules/BuatLaporanModule/sections/LaporanSection.tsx
+++ src/app/components/modules/BuatLaporanModule/sections/LaporanSection.tsx
@@ -1,8 +1,50 @@
+import Stack from "@/app/components/elements/Stack"
 import SubmitLaporanElement from "../element/SubmitLaporanElement"
 
 const LaporanSection = ()=> {
-    return <div>
+    return <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full mt-8 space-y-4">
+        <div className="w-full flex flex-col space-y-1">
+            <label className="text-lg text-black font-semibold">Nama Pelapor</label>
+            <input placeholder="Tulis nama lengkap Anda" 
+            className="w-full text-sm  text-black border-2 pr-2 pl-8 py-3 border-blue-400 rounded-md"/>
+        </div>
+        <div className="w-full flex flex-col space-y-1">
+            <label className="text-lg text-black font-semibold">Jenis Tindakan Kriminal</label>
+            <Stack>
+            <input  placeholder="Tulis tanggal kejadian" 
+            className="w-full text-sm text-black border-2 pr-2 pl-10 py-3 border-blue-400 rounded-md"/>
+            <img src="./calendar.svg" className="my-auto ml-auto mr-4" />
+            </Stack>
+        </div>
+        <div className="w-full flex flex-col space-y-1">
+            <label className="text-lg text-black font-semibold">Waktu Kejadian</label>
+            <Stack>
+            <input  placeholder="Tulis waktu kejadian" 
+            className="w-full text-sm text-black border-2 pr-2 pl-10 py-3 border-blue-400 rounded-md"/>
+            <img src="./clock.svg" className="my-auto ml-4" />
+            </Stack>
+        </div>
+        <div className="w-full flex flex-col space-y-1">
+            <label className="text-lg text-black font-semibold">Tanggal Kejadian</label>
+            <Stack>
+            <input  placeholder="Tulis tanggal kejadian" 
+            className="w-full text-sm text-black border-2 pr-2 pl-10 py-3 border-blue-400 rounded-md"/>
+            <img src="./calendar.svg" className="my-auto ml-4" />
+            </Stack>
+        </div>
+        <div className="w-full flex flex-col space-y-1">
+            <label className="text-lg text-black font-semibold">Lokasi Kejadian</label>
+            <input placeholder="Tulis detail lokasi kejadian" 
+            className="w-full text-sm  text-black border-2 pr-2 pl-8 py-3 border-blue-400 rounded-md"/>
+        </div>
+        <div className="w-full flex flex-col space-y-1">
+            <label className="text-lg text-black font-semibold">Deskripsi</label>
+            <input placeholder="Ceritakan kejadian dengan rinci" 
+            className="w-full text-sm  text-black border-2 pr-2 pl-8 py-3 border-blue-400 rounded-md"/>
+        </div>
 
+    </div>
         <SubmitLaporanElement/>
     </div>
 }
