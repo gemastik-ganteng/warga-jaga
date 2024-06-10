6c0c98e24428ddc443609a3671551e45de7691d1
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-06-10 21:54:56 +0700

    feat: notifikasi

---
+++ src/app/notifikasi/page.tsx
@@ -0,0 +1,5 @@
+import NotifikasiModule from "../components/modules/NotifikasiModule";
+
+export default function Page() {
+  return <NotifikasiModule />;
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,17 +1,21 @@
 export interface Laporan {
-    namaPelapor: string,
-    jenisTindakan: string,
-    waktuKejadian: string,
-    tanggalKejadian: string,
-    lokasiKejadian: string,
-    deskripsiKejadian: string,
-    bukti: Bukti[]
-
+  namaPelapor: string;
+  jenisTindakan: string;
+  waktuKejadian: string;
+  tanggalKejadian: string;
+  lokasiKejadian: string;
+  deskripsiKejadian: string;
+  bukti: Bukti[];
 }
 
-
 export interface Bukti {
-    name: string;
-    type: string;
-    base64: string;
+  name: string;
+  type: string;
+  base64: string;
+}
+
+export interface Notifikasi {
+  tanggal: string;
+  jam: string;
+  deskripsi: string;
 }
