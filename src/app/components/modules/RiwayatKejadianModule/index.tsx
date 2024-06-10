82b3eb95034ccf75abfdfd0ec2f7dd85849fd0ff
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-06-10 14:00:17 +0700

    feat: riwayat-kejadian page

--- src/app/components/modules/RiwayatKejadianModule/index.tsx
+++ src/app/components/modules/RiwayatKejadianModule/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import HeaderSection from "./section/HeaderSection";
+import RiwayatTileSection from "./section/RiwayatTileSection";
 
 const RiwayatKejadianModule = () => {
   return (
-    <div className="w-screen min-h-screen flex flex-col justify-center bg-white">
-      
-    </div>
+      <div className="w-screen min-h-screen flex flex-col justify-start bg-white">
+        <HeaderSection />
+        <RiwayatTileSection />
+      </div>
   );
 };
 
