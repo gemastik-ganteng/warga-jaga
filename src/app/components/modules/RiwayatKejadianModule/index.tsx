22228acd567f0ad9d98bdf6ec628f486cbaff255
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-06-10 12:34:02 +0700

    feat: riwayat

---
+++ src/app/components/modules/RiwayatKejadianModule/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+const RiwayatKejadianModule = () => {
+  return (
+    <div className="w-screen min-h-screen flex flex-col justify-center bg-white">
+      
+    </div>
+  );
+};
+
+export default RiwayatKejadianModule;
